c7725e514e9c0c4db1d4b2fb35a1b887cecd9662
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-08-16 16:48:27 -0300

    att

---
+++ lib/models/Chat_user.dart
@@ -0,0 +1,12 @@
+class Chat_user {
+  final String id;
+  final String name;
+  final String email;
+  final String ImageUrl;
+
+  const Chat_user(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.ImageUrl});
+}
